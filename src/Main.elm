dac45fe1d187526b6bab5504c54da0bd2a61014e
Matt Burke <spraints@gmail.com>
2018-02-20 14:21:13 -0500

    Edit days finished/required

--- src/Main.elm
+++ src/Main.elm
@@ -2,15 +2,19 @@ port module Main exposing (..)
 
 import Date exposing (Date)
 import Html
+import Html.Attributes
+import Html.Events
 
-type Msg = UpdateDaysFinished Int
-         | UpdateDaysRequired Int
+type alias ParsedInt = Result String Int
+
+type Msg = UpdateDaysFinished (String, ParsedInt)
+         | UpdateDaysRequired (String, ParsedInt)
          | SkipDay Date
          | UnskipDay Date
 
 type alias Model =
-  { days_finished : Int
-  , days_required : Int
+  { days_finished : (String, ParsedInt)
+  , days_required : (String, ParsedInt)
   , days_to_skip : List Date }
 
 port title : String -> Cmd a
@@ -25,7 +29,14 @@ main = Html.program
 -- days left
 init : (Model, Cmd Msg)
 init =
-  ({ days_finished = 0, days_required = 180, days_to_skip = [] }, title "School Days Remaining")
+  let
+    initialModel =
+      { days_finished = ("0", Ok 0)
+      , days_required = ("180", Ok 180)
+      , days_to_skip = []
+      }
+  in
+    (initialModel, title "School Days Remaining")
 
 sub : Model -> Sub Msg
 sub model = Sub.none
@@ -44,4 +55,32 @@ update msg model =
 
 view : Model -> Html.Html Msg
 view model =
-  Html.text "todo"
+  Html.div []
+    [ configView model
+    , calendarView model
+    ]
+
+configView : Model -> Html.Html Msg
+configView model =
+  let
+    configLine prompt (s, val) mkmsg =
+      Html.tr []
+        [ Html.td [] [ Html.text prompt ]
+        , Html.td [] [ Html.input [ Html.Events.onInput <| updateConfig mkmsg, Html.Attributes.value s ] [] ]
+        , Html.td [] [ configError val ]
+        ]
+    configError val =
+      case val of
+        Ok _ -> Html.text ""
+        Err msg -> Html.span [ Html.Attributes.class "config-error" ] [ Html.text msg ]
+    updateConfig mkmsg s =
+      mkmsg (s, String.toInt s)
+  in
+    Html.table []
+      [ configLine "Days Finished" model.days_finished UpdateDaysFinished
+      , configLine "Days Required" model.days_required UpdateDaysRequired
+      ]
+
+calendarView : Model -> Html.Html Msg
+calendarView model =
+  Html.text "todo: calendar view"
