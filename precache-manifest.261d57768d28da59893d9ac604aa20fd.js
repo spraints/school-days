4dad9afc4d86b724b346ef7c48a073d74bcac9bc
Matt Burke <spraints@gmail.com>
2019-06-06 18:44:23 -0400

    Publish bfb8ec89bfc06a6bf90243c1cd15668134a18def

---
+++ precache-manifest.261d57768d28da59893d9ac604aa20fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ffb23b48c67ee9151b1494596afdb6",
+    "url": "/school-days/index.html"
+  },
+  {
+    "revision": "eaf8f16a52a62300ae17",
+    "url": "/school-days/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "5fdc11b96e20f5d73ef0",
+    "url": "/school-days/static/css/main.7f9ad7a2.chunk.css"
+  },
+  {
+    "revision": "eaf8f16a52a62300ae17",
+    "url": "/school-days/static/js/2.b0f4c8b7.chunk.js"
+  },
+  {
+    "revision": "5fdc11b96e20f5d73ef0",
+    "url": "/school-days/static/js/main.74c610af.chunk.js"
+  },
+  {
+    "revision": "bef2047a1e528338c7eb",
+    "url": "/school-days/static/js/runtime~main.6384ee24.js"
+  }
+]);
